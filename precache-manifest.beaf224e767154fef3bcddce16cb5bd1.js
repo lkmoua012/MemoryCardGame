0e85da6544ee41afaf2eb79c25a7e8f814783510
larry.moua <lkmoua@gmail.com>
2018-10-20 20:09:36 -0700

    Updates

---
+++ precache-manifest.beaf224e767154fef3bcddce16cb5bd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e10e8f58cc5d5e33448a",
+    "url": "/MemoryCardGame/static/js/runtime~main.e10e8f58.js"
+  },
+  {
+    "revision": "00e5416a68e511286570",
+    "url": "/MemoryCardGame/static/js/main.00e5416a.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/MemoryCardGame/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "00e5416a68e511286570",
+    "url": "/MemoryCardGame/static/css/main.1fbc266d.chunk.css"
+  },
+  {
+    "revision": "8abdd340cf79f202f82d676c3aade5d7",
+    "url": "/MemoryCardGame/index.html"
+  }
+];
